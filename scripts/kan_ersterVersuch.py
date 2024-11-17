574a90d99d6db5c5c89abd4b409c705e1efe7213
leasche <lea.scherer1@gmail.com>
2024-11-17 13:03:35 +0100

    Einrueckungen waren falsch, funktionen waren nicht richtig in der classe KANModel

--- scripts/kan_ersterVersuch.py
+++ scripts/kan_ersterVersuch.py
@@ -7,94 +7,71 @@ import yaml
 import sys
 from datetime import datetime
 
-#tfkan muss im conda env installiert sein
-#importieren genau wie im github beschrieben
+# tfkan muss im conda env installiert sein
 from tfkan import layers
 from tfkan.layers import DenseKAN, Conv2DKAN
-from callbacks import TrainingPlot  
+from callbacks import TrainingPlot
 
-"""
-Wie in train.py wird als erstes Argument ein
- yaml config file uebergeben 
- (das Beispielfile hab ich in config gestellt). 
- train kann als zweites argument gegeben werden 
-um das trainieren direkt zu starten
-"""
+
 class KANModel:
     def __init__(self, path):
         """
         KAN model wird mit den Parametern aus dem config file initialisiert
         """
         try:
-            #config file laden:
             with open(path, 'r') as stream:
                 try: 
                     data = yaml.safe_load(stream)
-                    #parameter aus dem config file entnehmen:
                     try: 
                         timestamp = self.datetime_stamp()
                         self.dropout = data['dropout'] 
-                        self.layers = data['layers']  
+                        self.layers = data['layers']
                         self.learning_rate = data['learning_rate']
-                        self.batch_size = data['batch_size'] 
-                        self.epochs = data['epochs'] 
-                        self.no_epochs_dataset = data['no_epochs_dataset']  
-                        self.perc_eval = data['perc_eval']  
-                        self.data_file = data['data_file']  
-                        self.data_length = data['data_length'] 
-                        self.save_network_to = f"../models/{data['save_network_to']}_{timestamp}" if 'save_network_to' in data \
-                                else None
-                        self.output_file = f"../log/{data['save_network_to']}_{timestamp}.log" if 'save_network_to' in data \
-                                else None
-                        self.weight_initializer = data['weight_initializer'] if 'weight_initializer' in data \
-                                else 'xavier'
-                        self.transfer_function = data['transfer_function'] if 'transfer_function' in data else 'relu'
-                        self.activation_function = data['activation_function'] if 'activation_function' in data \
-                                else 'sigmoid'
-                        self.cost_function = data.get('cost_function', 'binary_crossentropy') 
-                        self.optimizer_type =  self.opt = data['optimizer'] if 'optimizer' in data else 'adam'
-                        self.offset = data['offset']  
-                        self.shuf_buffer = data['shuf_buffer'] if 'shuf_buffer' in data else 1000
-                        self.seed_init = data['seed_init'] if 'seed_init' in data else None
-                        self.seed_shuffle = data['seed_shuffle'] if 'seed_shuffle' in data else None
-                        self.beta1 = data['beta1'] if 'beta1' in data else 0.9
-                        self.beta2 = data['beta2'] if 'beta2' in data else 0.999
-                        self.use_bias = data['use_bias'] if 'use_bias' in data else True
+                        self.batch_size = data['batch_size']
+                        self.epochs = data['epochs']
+                        self.no_epochs_dataset = data['no_epochs_dataset']
+                        self.perc_eval = data['perc_eval']
+                        self.data_file = data['data_file']
+                        self.data_length = data['data_length']
+                        self.save_network_to = f"../models/{data['save_network_to']}_{timestamp}" if 'save_network_to' in data else None
+                        self.output_file = f"../log/{data['save_network_to']}_{timestamp}.log" if 'save_network_to' in data else None
+                        self.weight_initializer = data.get('weight_initializer', 'xavier')
+                        self.transfer_function = data.get('transfer_function', 'relu')
+                        self.activation_function = data.get('activation_function', 'sigmoid')
+                        self.cost_function = data.get('cost_function', 'binary_crossentropy')
+                        self.optimizer_type = data.get('optimizer', 'adam')
+                        self.offset = data['offset']
+                        self.shuf_buffer = data.get('shuf_buffer', 1000)
+                        self.seed_init = data.get('seed_init', None)
+                        self.seed_shuffle = data.get('seed_shuffle', None)
+                        self.beta1 = data.get('beta1', 0.9)
+                        self.beta2 = data.get('beta2', 0.999)
+                        self.use_bias = data.get('use_bias', True)
                         
-                        self.log_level = data['log_level'] if 'log_level' in data else 'info'
-                        tf.compat.v1.logging.set_verbosity({'debug': 10, 'error': 40, 'fatal': 50, 'info': 20, 'warn': 30}
-                                                    [data['log_level']] if 'log_level' in data else 20)
+                        self.log_level = data.get('log_level', 'info')
+                        tf.compat.v1.logging.set_verbosity({'debug': 10, 'error': 40, 'fatal': 50, 'info': 20, 'warn': 30}.get(data['log_level'], 20))
                     except KeyError as e:
-                            logging.error("Key error. Please refer to config file spec for more details", e)
-                            exit(1)
-
+                        logging.error("Key error. Please refer to config file spec for more details", e)
+                        exit(1)
                 except yaml.scanner.ScannerError as e:
-                    logging.error("yaml config is not valid. Please follow spec and provide valid yaml.", e)
+                    logging.error("yaml config is not valid. Please provide valid yaml.", e)
                     exit(1)
         except FileNotFoundError:
             logging.error("Config file not found")
             exit(1)
 
-        #logging
         self.logger = self.get_logger()
         self.logger.info("KAN initialized with parameters: " + str(data))
 
     def datetime_stamp(self):
-        t = datetime.now()
-        t = str(t)
-        t = t.split('.')[0]
-        t = t.replace(' ', '_')
-        t = t.replace('-', '')
-        t = t.replace(':', '')
+        t = datetime.now().strftime('%Y%m%d_%H%M%S')
         return t
 
-
     def get_logger(self):
-       
         logger = logging.getLogger('training')
         fh = logging.FileHandler(self.output_file)
         sh = logging.StreamHandler(sys.stdout)
-        formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s', datefmt = '%d.%m.%y %H:%M:%S')
+        formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s', datefmt='%d.%m.%y %H:%M:%S')
         fh.setFormatter(formatter)
         sh.setFormatter(formatter)
         logger.addHandler(fh)
@@ -103,12 +80,12 @@ class KANModel:
         return logger
 
     def parse_row(self, tf_string):
-        data = tf.io.decode_csv(tf.expand_dims(tf_string, axis = 0), self.get_record_defaults())
+        data = tf.io.decode_csv(tf.expand_dims(tf_string, axis=0), self.get_record_defaults())
         features = data[self.offset:-1]
-        features = tf.stack(features, axis = -1)
+        features = tf.stack(features, axis=-1)
         label = data[-1]
-        features = tf.squeeze(features, axis = 0)
-        label = tf.squeeze(label, axis = 0)
+        features = tf.squeeze(features, axis=0)
+        label = tf.squeeze(label, axis=0)
         return features, label
 
     def create_dataset(self):
@@ -122,109 +99,63 @@ class KANModel:
         train_data = train_data.map(self.parse_row, num_parallel_calls=6)
         val_data = val_data.map(self.parse_row, num_parallel_calls=6)
 
-        iterator_train = iter(train_data)
-        iterator_val = iter(val_data)
-
-        return iterator_train, iterator_val
-    
-    def cost_function(self):
-        if self.cost_f == 'sigmoid cross-entropy':
-            self.loss = 'binary_crossentropy'
-        elif self.cost_f == 'MSE':
-            self.loss = 'mean_squared_error'
-        else:
-            self.logger.error(f"Cost function {self.cost_f} not implemented.")
-            exit(1)
+        return iter(train_data), iter(val_data)
 
     def get_activation(self):
         if self.activation_function == 'sigmoid':
-            activation = tf.nn.sigmoid
+            return tf.nn.sigmoid
         elif self.activation_function == 'softmax':
-            activation = tf.nn.softmax
+            return tf.nn.softmax
         elif self.activation_function == 'no':
-            activation = None
+            return None
         else:
-            self.logger.error(f'Activation function {self.activation_function} is not implemented')
+            self.logger.error(f'Activation function {self.activation_function} not implemented')
             exit(2)
-        return activation
-    
+
     def get_transfer(self):
         if self.transfer_function == 'sigmoid':
-            transfer = tf.nn.sigmoid
+            return tf.nn.sigmoid
         elif self.transfer_function == 'relu':
-            transfer = tf.nn.relu
+            return tf.nn.relu
         else:
-            self.logger.error(f'Transfer function {self.transfer_function} is not implemented')
+            self.logger.error(f'Transfer function {self.transfer_function} not implemented')
             exit(2)
-        return transfer
 
-def build_kan_model(self):
-    """
-    KAN model mit tfkan bauen: 
-    """
-    self.model = keras.Sequential()
-    
-    for layer_config in self.layers:
-        layer_type = layer_config['type']
-        units = layer_config.get('units', 32)
-        activation = self.get_activation()
+    def build_kan_model(self):
+        self.model = keras.Sequential()
+        for layer_config in self.layers:
+            layer_type = layer_config['type']
+            units = layer_config.get('units', 32)
+            activation = self.get_activation()
 
-        if layer_type == 'dense':
-            self.model.add(DenseKAN(units=units, activation=activation, 
-                                    kernel_initializer=self.weight_initializer, 
-                                    use_bias=self.use_bias))
-        elif layer_type == 'conv2d':
-            filters = layer_config.get('filters', 32)
-            kernel_size = layer_config.get('kernel_size', (3, 3))
-            self.model.add(Conv2DKAN(filters=filters, kernel_size=kernel_size, activation=activation, 
-                                     kernel_initializer=self.weight_initializer, 
-                                     use_bias=self.use_bias))
-        if self.dropout > 0:
-            self.model.add(keras.layers.Dropout(self.dropout))
-    
-    #letzte Schicht (Ausgabeschicht)
-    self.model.add(DenseKAN(units=1, activation='sigmoid'))  
-    self.logger.info("KAN model architecture built")
+            if layer_type == 'dense':
+                self.model.add(DenseKAN(units=units, activation=activation, kernel_initializer=self.weight_initializer, use_bias=self.use_bias))
+            elif layer_type == 'conv2d':
+                filters = layer_config.get('filters', 32)
+                kernel_size = layer_config.get('kernel_size', (3, 3))
+                self.model.add(Conv2DKAN(filters=filters, kernel_size=kernel_size, activation=activation, kernel_initializer=self.weight_initializer, use_bias=self.use_bias))
+            if self.dropout > 0:
+                self.model.add(keras.layers.Dropout(self.dropout))
+        
+        self.model.add(DenseKAN(units=1, activation='sigmoid'))
+        self.logger.info("KAN model architecture built")
 
     def compile_model(self):
-        """
-        Kompilieren des KAN models mit dem optimizer und loss function aus dem Config
-        """
         if self.optimizer_type == "adam":
-            optimizer = Adam(learning_rate=self.learning_rate, beta_1 = self.beta1, beta_2 = self.beta2, amsgrad = False)
+            optimizer = Adam(learning_rate=self.learning_rate, beta_1=self.beta1, beta_2=self.beta2, amsgrad=False)
         else:
             self.logger.error(f"Optimizer {self.optimizer_type} not implemented.")
             exit(1)
 
-        self.model.compile(
-            optimizer=optimizer,
-            loss=self.cost_function,
-            metrics=['accuracy']
-        )
-        self.logger.info("Model compiled with optimizer and loss function")
-
-
-    def get_model_summary(self):
-        summary = []
-        self.model.summary(print_fn = lambda x: summary.append(x))
-        return "\n".join(summary)
+        self.model.compile(optimizer=optimizer, loss=self.cost_function, metrics=['accuracy'])
+        self.logger.info("Model compiled")
 
     def train(self):
-        """
-        trainieren mit den Daten 
-        """
         train_it, val_it = self.create_dataset()
-        
-        #build and compile the model 
         self.build_kan_model()
         self.compile_model()
 
-        model_checkpoint = ModelCheckpoint(filepath = self.save_network_to + '_{epoch:03d}-{binary_accuracy:.3f}-{val_binary_accuracy:.3f}', 
-                                           monitor = 'val_binary_accuracy', 
-                                           mode = 'max', 
-                                           save_best_only = False, 
-                                           verbose = 1)
-        
+        model_checkpoint = ModelCheckpoint(filepath=self.save_network_to + '_{epoch:03d}-{accuracy:.3f}-{val_accuracy:.3f}', monitor='val_accuracy', mode='max', save_best_only=False, verbose=1)
         reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.8, patience=5, verbose=1)
         early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
         plot_curves = TrainingPlot(self.save_network_to, self.logger)
@@ -232,7 +163,6 @@ def build_kan_model(self):
         steps_per_epoch = int(self.data_length * (1 - self.perc_eval) / self.batch_size / self.no_epochs_dataset)
         val_steps = int(self.data_length * self.perc_eval / self.batch_size)
 
-        
         self.model.fit(
             train_it,
             validation_data=val_it,
@@ -244,12 +174,11 @@ def build_kan_model(self):
         )
         self.logger.info("Training beendet")
 
+
 if __name__ == '__main__':
     try:
-        #read the config file path from command line arguments
         kan_model = KANModel(sys.argv[1])
-        #train network
         kan_model.train()
-    except IndexError as e:
-        logging.error("Please specify a path to a config file as first commandline argument")
+    except IndexError:
+        logging.error("Please specify a path to a config file as the first command line argument")
         exit(1)
