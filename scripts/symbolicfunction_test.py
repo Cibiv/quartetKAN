916e80464310216de4244812c65ff872a367220a
leasche <148796330+leasche@users.noreply.github.com>
2025-01-20 14:18:23 +0100

    Create symbolicfunction_test.py

---
+++ scripts/symbolicfunction_test.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import tensorflow.keras as keras
+
+import argparse
+import logging
+import os
+
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+from pylab import *
+
+matplotlib.use('Agg')
+
+# pass arguments with flags
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-m", "--model", help = "Model to test.")
+parser.add_argument("-t", "--test", default = '../data/processed/zone/test/1000bp', help = "Path to test directory")
+parser.add_argument("-l", "--seqlen", default = '1000bp', help = "Sequence Length")
+parser.add_argument("-o", "--offset", type = int, default = 4, help = "Index of first feature column.")
+parser.add_argument("-n", "--no_feat", type = int, default = 15, help = "Number of feature columns.")
+args = vars(parser.parse_args())
+
+model = keras.saving.load_model(args['model'])
+
+#model.plot()
+#model.symbolic_formula()[0][0]
+
+print(model.plot())
+print(model.symbolic_formula()[0][0])
