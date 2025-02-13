cfb43d5cffd8b7e5a19b092839df8980eeca37ab
leasche <148796330+leasche@users.noreply.github.com>
2025-02-13 19:17:06 +0100

    Delete scripts/bash_train_zwei.sh

--- scripts/bash_train_zwei.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python kan_drei.py ../config/config_lr3.yaml
-python kan_drei.py ../config/config_lr4.yaml
-python kan_drei.py ../config/config_lr7.yaml
-python kan_drei.py ../config/config_lr8.yaml
-python kan_drei.py ../config/config_lr9.yaml
-python kan_drei.py ../config/config_lr10.yaml
